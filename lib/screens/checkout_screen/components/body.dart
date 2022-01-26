f0e3cf8a0adcdb4ad7b41cf92eb8cf2825a80f5f
amjed <sasukeamjed@gmail.com>
2022-01-26 13:47:28 +0400

    adding an address box

--- lib/screens/checkout_screen/components/body.dart
+++ lib/screens/checkout_screen/components/body.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+
+
+  bool checkAddress = true;
 
   //Body of Checkout Page
   @override
@@ -28,17 +30,39 @@ class Body extends StatelessWidget {
         ),
         Padding(
           padding: EdgeInsets.only(left: 15.w),
-          child: DottedBorder(
-            color: Color(0xFF0C66FF),
-            strokeWidth: 3,
-            dashPattern: [12,6],
-            child: Container(
-              height: 70.h,
-              width: 310.w,
-            ),
-          ),
+          child: checkAddress ? EmptyAddress() : SizedBox(),
         ),
       ],
+    );
+  }
+}
+
+class EmptyAddress extends StatelessWidget {
+  const EmptyAddress({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DottedBorder(
+      color: Color(0xFF0C66FF).withOpacity(0.9),
+      strokeWidth: 3,
+      dashPattern: [12, 6],
+      child: Container(
+        height: 70.h,
+        width: 310.w,
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 5,
+              ),
+              Text("Add your address here", style: TextStyle(fontFamily: "Avenir-Book", fontSize: 14.sp, color: Colors.black.withOpacity(0.4),),),
+              Icon(Icons.add, color: Colors.black.withOpacity(0.4),),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
