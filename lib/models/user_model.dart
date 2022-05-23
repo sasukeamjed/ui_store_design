6da9719e8ab94768825a3820879347a09499e98a
sasuk <sasukeamjed@gmail.com>
2022-05-24 00:25:27 +0400

    adding services to the app

---
+++ lib/models/user_model.dart
@@ -0,0 +1,8 @@
+class UserModel{
+
+  // final String email;
+  // final String firstName;
+  // final String lastName;
+  // final String phoneNumber;
+
+}
--- lib/services/data/data.dart
+++ lib/services/data/data.dart
@@ -2,4 +2,5 @@ import 'package:ui_store_design/models/product.dart';
 
 abstract class Data{
   List<Product> fetchAllProducts();
+
 }
