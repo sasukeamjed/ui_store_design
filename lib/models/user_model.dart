a83d43c2eb6a04bc4b430e199248b353ed412c8b
sasuk <sasukeamjed@gmail.com>
2022-06-05 19:42:56 +0400

    generating user filef from json seriazible package

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,13 +1,30 @@
 import 'package:ui_store_design/models/shipping_model.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class UserModel{
 
+  @JsonKey(name: 'id')
   final String userID;
-  final String email;
-  final String firstName;
-  final String lastName;
-  final String phoneNumber;
-  final ShippingModel shippingModel;
 
-  UserModel(this.userID, this.email, this.firstName, this.lastName, this.phoneNumber, this.shippingModel);
+  @JsonKey(name: 'email')
+  final String email;
+
+  @JsonKey(name: 'first_name')
+  final String firstName;
+
+  @JsonKey(name: 'last_name')
+  final String lastName;
+
+  @JsonKey(name: 'username')
+  final String username;
+
+  // final ShippingModel shippingModel;
+
+  UserModel(this.userID, this.email, this.firstName, this.lastName, this.username);
+
+  factory UserModel.fromJson(Map<String, dynamic> json)=> _$UserModelFromJson(json);
+  Map<String, dynamic> toJson => _$UserModelToJson(this);
 }
