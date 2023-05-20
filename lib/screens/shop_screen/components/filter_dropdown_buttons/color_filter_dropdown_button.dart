a8dbcd28f7d1c61ec0d6d3684d785d2a0a0e549f
sasuk <sasukeamjed@gmail.com>
2023-05-20 12:58:50 +0400

    Storing main filter method in a provider

--- lib/screens/shop_screen/components/filter_dropdown_buttons/color_filter_dropdown_button.dart
+++ lib/screens/shop_screen/components/filter_dropdown_buttons/color_filter_dropdown_button.dart
@@ -12,17 +12,16 @@ class ColorFilterDropDownButton extends ConsumerStatefulWidget {
   const ColorFilterDropDownButton({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ColorFilterDropDownButton> createState() => _ColorFilterDropDownButtonState();
+  ConsumerState<ColorFilterDropDownButton> createState() =>
+      _ColorFilterDropDownButtonState();
 }
 
-class _ColorFilterDropDownButtonState extends ConsumerState<ColorFilterDropDownButton> {
-
-
-
-
+class _ColorFilterDropDownButtonState
+    extends ConsumerState<ColorFilterDropDownButton> {
   @override
   Widget build(BuildContext context) {
-    final Set<String> colors = ref.read(productsDataProvider.notifier).getColors();
+    final Set<String> colors =
+        ref.read(productsDataProvider.notifier).getColors();
     return TextButton(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -61,100 +60,111 @@ class _ColorFilterDropDownButtonState extends ConsumerState<ColorFilterDropDownB
           surfaceTintColor: Colors.green,
           shadowColor: Colors.green,
           foregroundColor: Colors.grey
-        // foregroundColor: Colors.black,
-      ),
-    );;
+          // foregroundColor: Colors.black,
+          ),
+    );
+    ;
   }
 
   void openFilterDialog(Set<String> colorsNames) async {
-    List<String>? selectedColors = [];
-
+    List<String> selectedColors = ref.read(colorFilterProvider);
 
     await FilterListDialog.display<String>(
       context,
-
-      controlButtons: [ControlButtonType.Reset],
       listData: colorsNames.toList(),
       selectedListData: ref.read(colorFilterProvider),
+      // selectedListData: ref.read(colorFilterProvider).toSet().toList(),
       choiceChipLabel: (color) => color,
-      validateSelectedItem: (list, val) => list!.contains(val),
+      validateSelectedItem: (list, val) {
+        return ref.read(colorFilterProvider).contains(val);
+      },
       onItemSearch: (color, query) {
         return color.toLowerCase().contains(query.toLowerCase());
       },
       choiceChipBuilder: (context, colorName, isClicked) {
-        if(isClicked != null){
-          if(isClicked){
-            // print("$colorName filter is clicked selected colors");
-            // print("$selectedColors this is selected colors");
-            // print("${ref.read(colorFilterProvider)} Selected Colors before condition");
-            // print("${ref.read(colorFilterProvider).contains(colorName)} color condition");
-            // if(ref.read(colorFilterProvider).contains(colorName)){
-            //   ref.read(colorFilterProvider).remove(colorName);
-            // }else{
-            //   ref.read(colorFilterProvider).add(colorName);
-            // }
-            //
-            // print("${ref.read(colorFilterProvider)} Selected Colors after condition");
-          }
-        }
+        // if(isClicked != null){
+        //   if(isClicked){
+        //     // print("$colorName filter is clicked selected colors");
+        //     // print("$selectedColors this is selected colors");
+        //     // print("${ref.read(colorFilterProvider)} Selected Colors before condition");
+        //     // print("${ref.read(colorFilterProvider).contains(colorName)} color condition");
+        //     // if(ref.read(colorFilterProvider).contains(colorName)){
+        //
+        //       if(!ref.read(colorFilterProvider).contains(colorName)){
+        //         ref.read(colorFilterProvider).add(colorName);
+        //       }
+        //
+        //
+        //
+        //     // }else{
+        //
+        //     // }
+        //     // //
+        //     // print("${ref.read(colorFilterProvider)} Selected Colors after condition");
+        //   }else{
+        //     // print("color is in the list removing a color");
+        //     // selectedColors.remove(colorName);
+        //     ref.read(colorFilterProvider).remove(colorName);
+        //
+        //   }
+        //
+        // }
+        // print("=====================================================");
+        // print("this $colorName is clicked => $isClicked");
+        // print("this is the list => ${ref.read(colorFilterProvider)}");
 
-        return GestureDetector(
-          onTap: (){
-            if(ref.read(colorFilterProvider).contains(colorName)){
-              ref.read(colorFilterProvider).remove(colorName);
-            }else{
-              ref.read(colorFilterProvider).add(colorName);
-            }
-          },
-          child: Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.topRight,
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.w),
-                height: 30,
-                width: 50,
-                child: Center(
-                  child: Text(
-                    colorName.toString(),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: getColorHexFromName(colorName),
-                  borderRadius: BorderRadius.circular(20),
-                  border: ref.watch(colorFilterProvider).contains(colorName)
-                      ? ref.watch(colorFilterProvider).contains(colorName)
-                      ? Border.all(color: Colors.orange.shade300, width: 2)
-                      : null
-                      : null,
+        print("this the selected colors is in the list => ${ref.read(colorFilterProvider)}");
+        // print("=====================================================");
+
+
+
+        return Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.topRight,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.w),
+              height: 30,
+              width: 50,
+              child: Center(
+                child: Text(
+                  colorName.toString(),
                 ),
               ),
-              if (isClicked != null)
-                if (ref.watch(colorFilterProvider).contains(colorName))
-                  Icon(
-                    Icons.close,
-                    color: Colors.orange.shade300,
-                    size: 10,
-                  ),
-            ],
-          ),
+              decoration: BoxDecoration(
+                color: getColorHexFromName(colorName),
+                borderRadius: BorderRadius.circular(20),
+
+                border: ref.watch(colorFilterProvider).contains(colorName) ? Border.all(color: Colors.orange.shade300, width: 2) : isClicked! ? Border.all(color: Colors.orange.shade300, width: 2) : null,
+                // border: ref.watch(colorFilterProvider).contains(colorName)
+                //     ? Border.all(color: Colors.orange.shade300, width: 2)
+                //     : null,
+              ),
+            ),
+            if(isClicked != null)
+              if (isClicked)
+                Icon(
+                  Icons.close,
+                  color: Colors.orange.shade300,
+                  size: 10,
+                ),
+          ],
         );
       },
-      onApplyButtonClick: (list) async{
+      onApplyButtonClick: (list) async {
         Navigator.pop(context);
         setState(() {
           selectedColors = List.from(list!);
-
         });
-        ref.read(colorFilterProvider.notifier).update((state) => selectedColors!);
+        ref.read(colorFilterProvider.notifier).update((state) => selectedColors);
         ref.read(shopScreenLoadingDataState.notifier).state = true;
 
         List<Product>? products = await ref.read(mainFilterMethod);
         ref.read(productsProvider.notifier).state = products!;
         ref.read(shopScreenLoadingDataState.notifier).state = false;
-
       },
-
     );
   }
+
+
 }
